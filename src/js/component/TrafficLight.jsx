84d6a624b612a8f190fa1496b74637e68a504656
Kasa95 <nachocasanova95@gmail.com>
2022-06-05 20:43:11 +0000

    Funcionando la lógica

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,32 +1,28 @@
 import React, { useState } from "react";
 
 const TrafficLight = () => {
-	const [highlight1, setHighlight1] = useState("");
-	const [highlight2, setHighlight2] = useState("");
-	const [highlight3, setHighlight3] = useState("");
-
-	function brillar1() {
-		highlight1 === "" ? setHighlight1("selected") : setHighlight1("");
-	}
-	function brillar2() {
-		highlight2 === "" ? setHighlight2("selected") : setHighlight2("");
-	}
-	function brillar3() {
-		highlight3 === "" ? setHighlight3("selected") : setHighlight3("");
-	}
+	const [selectedColor, setSelectedColor] = useState("");
 
 	return (
 		<>
 			<div className="trafficLight">
 				<button
-					className={"red " + highlight1}
-					onClick={brillar1}></button>
+					className={
+						"red " + (selectedColor === "red" ? "selected-red" : "")
+					}
+					onClick={() => setSelectedColor("red")}></button>
 				<button
-					className={"yellow " + highlight2}
-					onClick={brillar2}></button>
+					className={
+						"yellow " +
+						(selectedColor === "yellow" ? "selected-yellow" : "")
+					}
+					onClick={() => setSelectedColor("yellow")}></button>
 				<button
-					className={"green " + highlight3}
-					onClick={brillar3}></button>
+					className={
+						"green " +
+						(selectedColor === "green" ? "selected-green" : "")
+					}
+					onClick={() => setSelectedColor("green")}></button>
 			</div>
 		</>
 	);
